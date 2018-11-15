c262ec44b32d3351189a6d7ce02d1a19b0c7d72f
herve <herve.jouin@gmail.com>
2018-11-15 15:11:09 +0100

    reduce memory consumption

--- org.archicontribs.servicenow/src/org/archicontribs/servicenow/MyConnection.java
+++ org.archicontribs.servicenow/src/org/archicontribs/servicenow/MyConnection.java
@@ -1,7 +1,7 @@
 package org.archicontribs.servicenow;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Authenticator;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
@@ -38,6 +38,7 @@ public class MyConnection {
 	public String get(String what, String location, String username, String Password) throws MyException, IOException {
 		URL url = new URL(location);
 		HttpURLConnection c;
+        StringBuilder data = new StringBuilder();
 
 		if ( MyUtils.isSet(this.proxyHost) ) {
 			if ( MyUtils.isSet(this.proxyUser) ) {
@@ -62,31 +63,31 @@ public class MyConnection {
 			if ( status != 200) {
 				throw new MyException("Error reported by ServiceNow website : code " + Integer.toString(status)); 
 			}
-		} catch (Exception e) {
-			throw new MyException("Cannot connect to web site (" + e.getMessage() + ")"); 
+	
+    		try ( InputStreamReader reader = new InputStreamReader(c.getInputStream()) ) {
+    	        if ( this.logger != null ) this.logger.trace("      Getting " + what + " from ServiceNow webservice ...");
+    	        if ( this.progressBar != null ) this.progressBar.setLabel("Getting " + what + " from ServiceNow webservice ...");
+    	        
+    	        int nb=0, total=0;
+    	        char[] buffer = new char[10240];    // 10 KB
+    	        while ( (nb=reader.read(buffer,0,buffer.length)) > 0 ) {
+                    data.append(buffer,0,nb);
+                    total+=nb;
+                    if ( this.progressBar != null ) {
+                        if ( total < 1048576 ) {
+                            this.progressBar.setDetailLabel("read "+String.format("%d", total/1024) + " KB");
+                        } else {
+                            this.progressBar.setDetailLabel("read "+String.format("%.2f", (float)total/1048576) + " MB");
+                        }
+                    }
+                }
+                if ( this.logger != null ) this.logger.trace("      Read " + total + " bytes from ServiceNow webservice.");
+                if ( this.progressBar != null ) this.progressBar.setLabel("");
+            }
+		} finally {
+		    c.disconnect();
 		}
-
-		StringBuilder data = new StringBuilder();
-		try ( InputStream in = c.getInputStream() ) {
-			if ( this.logger != null ) this.logger.trace("      Getting " + what + " from ServiceNow webservice ...");
-			if ( this.progressBar != null ) this.progressBar.setLabel("Getting " + what + " from ServiceNow webservice ...");
-
-			int nb=0, total=0;
-			byte[] buffer = new byte[10240];    // 10 KB
-			while ( (nb=in.read(buffer,0,buffer.length)) > 0 ) {
-				data.append(new String(buffer,0,nb));
-				total+=nb;
-				if ( this.progressBar != null ) {
-					if ( total < 1048576 ) {
-						this.progressBar.setDetailLabel("read "+String.format("%d", total/1024) + " KB");
-					} else {
-						this.progressBar.setDetailLabel("read "+String.format("%.2f", (float)total/1048576) + " MB");
-					}
-				}
-			}
-			if ( this.logger != null ) this.logger.trace("      Read " + total + " bytes from ServiceNow webservice.");
-			if ( this.progressBar != null ) this.progressBar.setLabel("");
-		}
+		
 		return data.toString();
 	}
 }
